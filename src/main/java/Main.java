13e077219bd78d5de8466f1fbf6600bbf4599d05
Kevin Lee <klee03509@gmail.com>
2024-04-04 21:32:01 -0700

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +10,11 @@ public class Main {
             serverSocket.setReuseAddress(true);
 
             // Wait for connection from client.
-            try (var clientSocket = serverSocket.accept()) {
-                System.out.println("accepted new connection");
+            try (var clientSocket = serverSocket.accept();
+                 var outputStream = clientSocket.getOutputStream()) {
+
+                outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
             }
         } catch (IOException ioException) {
             System.out.println("IOException: " + ioException.getMessage());
