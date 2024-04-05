0c422ea76e508a46ef3b40f2d4a345f43273bb42
Kevin Lee <klee03509@gmail.com>
2024-04-04 21:29:15 -0700

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,19 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        var port = 4221;
 
-    // Uncomment this block to pass the first stage
-    //
-    // ServerSocket serverSocket = null;
-    // Socket clientSocket = null;
-    //
-    // try {
-    //   serverSocket = new ServerSocket(4221);
-    //   serverSocket.setReuseAddress(true);
-    //   clientSocket = serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
-  }
+        try (var serverSocket = new ServerSocket(port)) {
+            serverSocket.setReuseAddress(true);
+
+            // Wait for connection from client.
+            try (var clientSocket = serverSocket.accept()) {
+                System.out.println("accepted new connection");
+            }
+        } catch (IOException ioException) {
+            System.out.println("IOException: " + ioException.getMessage());
+        }
+    }
 }
